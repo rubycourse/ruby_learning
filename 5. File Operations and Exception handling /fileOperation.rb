637715ccd98553143b8812477317e55f5226f671
Roger Tong <Roger.Tong@activenetwork.com>
2015-02-16 16:00:24 +0800

    Rafactor #3
    1. make Hash definition as one line
    2. take use of block to make code tidy and clean

--- 5. File Operations and Exception handling /fileOperation.rb
+++ 5. File Operations and Exception handling /fileOperation.rb
@@ -1,16 +1,16 @@
 #Define a class which contains some common operations to file
 class FileOperator
+  OP = { c: "create", d: "delete", r: "read", a: "append" }
+  
+  def initialize(dir = "./")
+    setCurrDir(dir)
+  end
 
-  OP = { c: "create",
-    d: "delete",
-    r: "read",
-    a: "append" }
-
-  def new_file(name, path = "./")
-    print_start_info(OP[:c], name, path)
+  def operate(op, name, *content)
+    print_start_info(op, name)
 
     begin
-      file = File.new(path + name, "w")
+      file = yield(name, content)
     rescue StandardError => e
       puts e.to_s
     ensure
@@ -18,65 +18,60 @@ class FileOperator
     end
   end
 
-  def delete_file(name, path = "./")
-    print_start_info(OP[:d], name, path)
+  def new_file(name)
+    operate(OP[:c], name) { |name| File.new(name, "w") }
+  end
 
-    begin
-      File.delete(path + name)
-      puts "File #{name} is deleted successfully."
-    rescue StandardError => e
-      puts e.to_s
+  def delete_file(name)
+    operate(OP[:d], name) do |name| 
+      File.delete(name)
+      nil
     end
   end
 
-  def read_file(name, path = "./")
-    print_start_info(OP[:r], name, path)
-
-    begin
-      file = File.open(path + name, "r")
-      file.readlines
-    rescue StandarError => e
-      puts e.to_s
-    ensure
-      close_file(file)
-    end
+  def read_file(name)
+    operate(OP[:r], name) { |name| File.open(name, "r") }
   end
 
-  def append_file(content, name, path = "./")
-    print_start_info(OP[:a], name, path)
-
-    begin
-      file = File.open(path + name, "w")
+  def append_file(content, name)
+    operate(OP[:a], name) do |name, content|
+      file = File.open(name, "w")
       file.write(content)
-    rescue StandardError => e
-      puts e.to_s
-    ensure
-      close_file(file)
+      file
     end
+  end
+
+  def setCurrDir(dir)
+    puts "Input directory: #{dir}"
+    Dir.chdir(dir)
+    puts "Current Directory is #{Dir.pwd}"
   end
 
   private
   def close_file(file)
     if file
       file.close
-      puts "Files is closed"
+      puts "File is closed"
+    else
+      puts "File is removed or doens't exist"
     end
   end
   
-  def print_start_info(op, fileName, path)
-    puts "Start to #{op} a file named #{fileName} under #{path}"
+  def print_start_info(op, fileName)
+    puts "Start to #{op} a file named #{fileName}"
   end
 end
 
 file1 = "abc.txt"
 file2 = "def.txt"
-path = "/Users/rogertong/"
+dir1 = "/Users/Guest/"
+dir2 = "/Users/rogertong/"
 
-op = FileOperator.new
+op = FileOperator.new(dir1)
 
-op.new_file(file1, path)
-op.delete_file(file1, path)
+op.new_file(file1)
 
+op.setCurrDir(dir2)
 op.new_file(file2)
 op.append_file("hahaha", file2)
 op.read_file(file2)
