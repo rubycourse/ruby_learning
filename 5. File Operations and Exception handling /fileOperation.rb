60a636cbbedcfe2d61c015af863faee12a48641d
Roger Tong <Roger.Tong@activenetwork.com>
2015-02-16 09:47:20 +0800

    Refactor fileOperation.rb
    1. make file operation strings into Hash
    2. rename function names and use space as Ruby style
    3. take use of path parameter in every operation function
    4. print path info also when starting to operate on a file

--- 5. File Operations and Exception handling /fileOperation.rb
+++ 5. File Operations and Exception handling /fileOperation.rb
@@ -1,71 +1,78 @@
 #Define a class which contains some common operations to file
 class FileOperator
 
-  CREATE="create"
-  DELETE="delete"
-  READ="read"
-  APPEND="append"
+  OP = { "C" => "create",
+    "D" => "delete",
+    "R" => "read",
+    "A" => "append" }
 
-  def newFile(name, path="./")
-    printStartInfo(CREATE, name)
+  def new_file(name, path = "./")
+    print_start_info(OP["C"], name, path)
+
     begin
-      file = File.new(name, "w")
+      file = File.new(path + name, "w")
     rescue StandardError => e
       puts e.to_s
     ensure
-      closeFile(file)
+      close_file(file)
     end
   end
 
-  def deleteFile(name, path="./")
-    printStartInfo(DELETE, name)
+  def delete_file(name, path = "./")
+    print_start_info(OP["D"], name, path)
+
     begin
-      File.delete(path+name)
-      puts "File is deleted successfully."
+      File.delete(path + name)
+      puts "File #{name} is deleted successfully."
     rescue StandardError => e
       puts e.to_s
     end
   end
 
-  def readFile(name, path="./")
-    printStartInfo(READ, name)
+  def read_file(name, path = "./")
+    print_start_info(OP["R"], name, path)
+
     begin
-      file = File.open(path+name, "r")
+      file = File.open(path + name, "r")
       file.readlines
     rescue StandarError => e
       puts e.to_s
     ensure
-      closeFile(file)
+      close_file(file)
     end
   end
 
-  def appendFile(content, name, path="./")
-    printStartInfo(APPEND, name)
+  def append_file(content, name, path = "./")
+    print_start_info(OP["A"], name, path)
+
     begin
-      file = File.open(path+name, "w")
+      file = File.open(path + name, "w")
       file.write(content)
     rescue StandardError => e
       puts e.to_s
     ensure
-      closeFile(file)
+      close_file(file)
     end
   end
 
   private
-  def closeFile(file)
-    file.close
-    puts "Files is closed"
+  def close_file(file)
+    if file
+      file.close
+      puts "Files is closed"
+    end
   end
   
-  def printStartInfo(op, fileName)
-    puts "Start to #{op} a file named #{fileName}"
+  def print_start_info(op, fileName, path)
+    puts "Start to #{op} a file named #{fileName} under #{path}"
   end
 end
 
 op = FileOperator.new
-op.newFile("abc.txt")
-op.deleteFile("abc.txt")
-op.newFile("def.txt")
-op.appendFile("hahaha", "def.txt")
-op.readFile("def.txt")
-op.deleteFile("abc.txt")
+op.new_file("abc.txt", "/Users/rogertong/")
+op.delete_file("abc.txt", "/Users/rogertong/")
+op.new_file("def.txt")
+op.append_file("hahaha", "def.txt")
+op.read_file("def.txt")
+op.delete_file("abc.txt")
+
